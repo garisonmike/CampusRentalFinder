ccd056c8c8a0d643fc2432458719c9fe1642991c
garisonmike <garisonmike006@gmail.com>
2025-09-29 11:20:03 +0300

    added Footer.jsx

---
+++ frontend/src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+// frontend/src/components/layout/Footer.jsx
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white text-center p-4">
+            <p>&copy; {new Date().getFullYear()} CampusRentalFinder</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ frontend/src/services/store/authStore.js
@@ -0,0 +1,9 @@
+// ./services/store/authStore.js
+import { create } from 'zustand';
+
+const useAuthStore = create((set) => ({
+    user: null,
+    setUser: (user) => set({ user }),
+}));
+
+export default useAuthStore;
